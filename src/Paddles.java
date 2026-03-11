6f96eeaa76666196d40043503055ba11468c43c0
Caroline-Cuff <caroline_cuff28@milton.edu>
2026-03-11 09:07:19 -0400

    Initial commit

---
+++ src/Paddles.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class Paddles {
+
+
+    public int xpos;
+    public int ypos;
+    public int width;
+    public int height;
+    public boolean isAlive; //a boolean to denote if the hero is alive or dead.
+    public Rectangle hitbox;
+
+    public Paddles (int pxpos, int pypos){
+
+        xpos = pxpos;
+        ypos = pypos;
+        width =24;
+        height = 133;
+        isAlive = false;
+        hitbox = new Rectangle(xpos, ypos, width, height);
+    }
+
+}
